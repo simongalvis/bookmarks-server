354bb7d5c5987157c1c70e650213e28611c023ed
Simon Galvis <simongalvis9@gmail.com>
2020-11-17 00:23:15 -0500

    Create and populate bookmarks.fixtures.js

---
+++ test/bookmarks.fixtures.js
@@ -0,0 +1,40 @@
+function makeBookmarksArray(){
+  return [
+    {
+      id:1,
+      title:'First sample bookmark',
+      url:'www.firstsamplebookmark.com',
+      description:'This is the first sample bookmark',
+      rating:5,
+    },
+    {
+      id:2,
+      title:'Second sample bookmark',
+      url:'www.secondsamplebookmark.com',
+      description:'This is the second sample bookmark',
+      rating:5,
+    },
+    {
+      id:3,
+      title:'Third sample bookmark',
+      url:'www.thirdsamplebookmark.com',
+      description:'This is the third sample bookmark',
+      rating:5,
+    },
+    {
+      id:4,
+      title:'Fourth sample bookmark',
+      url:'www.fourthsamplebookmark.com',
+      description:'This is the fourth sample bookmark',
+      rating:5,
+    },
+    {
+      id:5,
+      title:'Fith sample bookmark',
+      url:'www.fithsamplebookmark.com',
+      description:'This is the fith sample bookmark',
+      rating:5,
+    },
+
+  ]
+}
